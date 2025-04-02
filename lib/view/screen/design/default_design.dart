cc62be24cbaadcb7c027021420f9d678e117486c
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-01 22:19:34 -0700

    first commit

---
+++ lib/view/screen/design/default_design.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DefaultDesign extends StatefulWidget {
+  const DefaultDesign({super.key});
+
+  @override
+  State<DefaultDesign> createState() => _DefaultDesignState();
+}
+
+class _DefaultDesignState extends State<DefaultDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
