10ba54a18ac13a61171c306747b7eb901ee759d6
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-06 22:22:51 -0700

    first commit

---
+++ lib/view/screen/design/design.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Design extends StatefulWidget {
+  const Design({super.key});
+
+  @override
+  State<Design> createState() => _DesignState();
+}
+
+class _DesignState extends State<Design> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
