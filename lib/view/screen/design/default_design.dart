fc5daba933f0be92c71f252c12848ca8a8696ac4
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-09 16:31:02 -0700

    first commit

--- lib/view/screen/design/default_design.dart
+++ lib/view/screen/design/default_design.dart
@@ -8,32 +8,12 @@ class DefaultDesign extends StatefulWidget {
 }
 
 class _DefaultDesignState extends State<DefaultDesign> {
-  final List<Map<String, dynamic>> data = [
-    {
-      "name": "fahim",
-      "roll": 123,
-      "dep": "cse",
-    },
-    {
-      "name": "siddik",
-      "roll": 456,
-      "dep": "cse",
-    },
-    {
-      "name": "sakib",
-      "roll": 789,
-      "dep": "cse",
-    },
-    {
-      "name": "milon",
-      "roll": 456,
-      "dep": "cse",
-    },
-    {
-      "name": "jamal",
-      "roll": 789,
-      "dep": "cse",
-    },
+  List data = [
+    {"name": "fahim", "roll": 123, "dep": "cse"},
+    {"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "sakib", "roll": 789, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"},
+    {"name": "jamal", "roll": 789, "dep": "cse"},
   ];
   @override
   Widget build(BuildContext context) {
@@ -41,25 +21,19 @@ class _DefaultDesignState extends State<DefaultDesign> {
       appBar: AppBar(
         title: Text("Design"),
       ),
-      body: Column(
-        children: [
-          GridView.builder(
-              itemCount: data.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (index, context) {
-                return Column(
-                  children: [
-                    Text(""),
-                    Text(""),
-                    Text(""),
-
-                  ],
-                );
-              })
-        ],
-      ),
+      body: GridView.builder(
+          itemCount: data.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context,index) {
+            return Column(
+              children: [
+                Text(data[index]["name"].toString()),
+                Text(data[index]["dep"].toString()),
+              ],
+            );
+          }),
     );
   }
 }
