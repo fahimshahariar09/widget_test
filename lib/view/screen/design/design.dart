d872dc1548ae4d34ba8c40e828c12ba09f2d1434
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-10 23:10:02 -0700

    first commit

--- lib/view/screen/design/design.dart
+++ lib/view/screen/design/design.dart
@@ -10,41 +10,52 @@ class Design extends StatefulWidget {
 class _DesignState extends State<Design> {
   bool check = false;
 
+  List data = [
+    {"name": "fahim", "roll": 123, "dep": "cse"},
+    {"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "sakib", "roll": 789, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"},
+    {"name": "jamal", "roll": 789, "dep": "cse"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Design"),
       ),
-      // body: Padding(
-      //   padding: const EdgeInsets.symmetric(horizontal: 10),
-      //   child: ListView.builder(itemBuilder: (context, index) {
-      //     return ListTile(
-      //       title: Text("Fahim"),
-      //       subtitle: Text("App"),
-      //       leading: CircleAvatar(
-      //         radius: 25,
-      //       ),
-      //     );
-      //   }),
-      // ),
-      body: Column(
-        children: [
-          Text("phone"),
-          TextField(),
-          Text("name"),
-          TextField(),
-          Text("password"),
-          TextField(),
-          Checkbox(
-              value: check,
-              onChanged: (bool ?value) {
-                setState(() {
-                  check=value!;
-                });
-              })
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ListView.builder(itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("Fahim"),
+            subtitle: Text("App"),
+            leading: CircleAvatar(
+              radius: 25,
+            ),
+          );
+        }),
       ),
+
+
+
+      // body: Column(
+      //   children: [
+      //     Text("phone"),
+      //     TextField(),
+      //     Text("name"),
+      //     TextField(),
+      //     Text("password"),
+      //     TextField(),
+      //     Checkbox(
+      //         value: check,
+      //         onChanged: (bool ?value) {
+      //           setState(() {
+      //             check=value!;
+      //           });
+      //         })
+      //   ],
+      // ),
     );
   }
 }
