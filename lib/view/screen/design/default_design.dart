75ced9e63bd0d58d032550b0124e05a876b71318
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-11 23:35:11 -0700

    first commit

--- lib/view/screen/design/default_design.dart
+++ lib/view/screen/design/default_design.dart
@@ -14,10 +14,6 @@ class _DefaultDesignState extends State<DefaultDesign> {
     {"name": "sakib", "roll": 789, "dep": "cse"},
     {"name": "milon", "roll": 456, "dep": "cse"},
     {"name": "jamal", "roll": 789, "dep": "cse"},
-    {"name": "jamal", "roll": 789, "dep": "cse"},
-    {"name": "jamal", "roll": 789, "dep": "cse"},
-    {"name": "jamal", "roll": 789, "dep": "cse"},
-    {"name": "jamal", "roll": 789, "dep": "cse"},
   ];
   @override
   Widget build(BuildContext context) {
