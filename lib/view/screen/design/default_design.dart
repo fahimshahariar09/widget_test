60c68a12d5169b25e3f15d6a6854139480ac661e
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-14 23:31:45 -0700

    first commit

--- lib/view/screen/design/default_design.dart
+++ lib/view/screen/design/default_design.dart
@@ -12,10 +12,15 @@ class _DefaultDesignState extends State<DefaultDesign> {
     {"name": "fahim", "roll": 123, "dep": "cse"},
     {"name": "siddik", "roll": 456, "dep": "cse"},
     {"name": "sakib", "roll": 789, "dep": "cse"},
-    {"name": "milon", "roll": 456, "dep": "cse"},{"name": "fahim", "roll": 123, "dep": "cse"},
-    {"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"}{"name": "siddik", "roll": 456, "dep": "cse"},
     {"name": "sakib", "roll": 789, "dep": "cse"},
-    {"name": "milon", "roll": 456, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"}{"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "sakib", "roll": 789, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"}{"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "sakib", "roll": 789, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"}{"name": "siddik", "roll": 456, "dep": "cse"},
+    {"name": "sakib", "roll": 789, "dep": "cse"},
+    {"name": "milon", "roll": 456, "dep": "cse"}
   ];
   @override
   Widget build(BuildContext context) {
@@ -41,7 +46,6 @@ class _DefaultDesignState extends State<DefaultDesign> {
                     Text(data[index]["name"].toString()),
                     Text(data[index]["dep"].toString()),
                     Text(data[index]["roll"].toString())
-
                   ],
                 ),
               ),
