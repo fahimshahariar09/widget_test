6744a6774b37f6d3fc479589aaca078fb9db4dc8
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-20 21:52:01 -0700

    first commit

---
+++ lib/view/screen/design/calender_screen.dart
@@ -0,0 +1,13 @@
+class CalenderScreen extends StatefulWidget {
+  const CalenderScreen({super.key});
+
+  @override
+  State<CalenderScreen> createState() => _CalenderScreenState();
+}
+
+class _CalenderScreenState extends State<CalenderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
