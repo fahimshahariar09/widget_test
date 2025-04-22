a8cb72a7daf79e5222f26fb4bf501fc92e61d0da
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-21 18:47:45 -0700

    first commit

--- lib/view/screen/practice/check_box.dart
+++ lib/view/screen/practice/check_box.dart
@@ -1,8 +1,15 @@
-class  extends StatelessWidget {
-  const ({super.key});
+import 'package:flutter/material.dart';
+
+class CheckBox extends StatefulWidget {
+  const CheckBox({super.key});
 
   @override
+  State<CheckBox> createState() => _CheckBoxState();
+}
+
+class _CheckBoxState extends State<CheckBox> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
