c991ed39f3d96b9d826c27a941b6638b10387945
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-24 22:01:33 -0700

    first commit

--- lib/view/screen/screen_dialog/add_show.dart
+++ lib/view/screen/screen_dialog/add_show.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class AddShow extends StatelessWidget {
   const AddShow({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
