cb807283bb1c83a7474646bce6339dfcfe4ffd8c
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-04-27 22:28:36 -0700

    first commit

--- lib/common_widget/custom_container_text.dart
+++ lib/common_widget/custom_container_text.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class CustomContainerText extends StatelessWidget {
   const CustomContainerText({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
